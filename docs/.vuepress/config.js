9a2e43a73c86d6ee88e08bdebd4fa89b0cf6436e
xiongqijiang <xiongqijiang-dc@139.com>
2023-02-21 15:10:54 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,57 @@
 // config.js
 module.exports = {
-    //路径问题
-    base: "/"
+    //base路径代表github仓库名称
+    base: "/",
+    title: 'MyReader',
+    description: 'md文档在线阅读器',
+    //主题参考https://vuepress-theme-reco.recoluan.com/views/1.x/installUse.html
+    theme: 'reco',
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        //md目录右侧显示
+        subSidebar: 'auto',
+        //顶部栏配置
+        nav: [
+            {text: '首页', link: '/'},
+            {
+                text: 'XDS 博客',
+                items: [
+                    {text: 'Github', link: 'https://github.com/xdsdao'},
+                    {text: 'CSDN', link: 'https://blog.csdn.net/xds666'}
+                ]
+            }
+        ],
+        // 侧边栏配置
+        sidebar: [
+            {
+                title: '导航栏',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    {title: "学前必读", path: "/"}
+                ]
+            },
+            {
+                title: "文章导航",
+                path: '/note/blog/blog_0001', // 默认激活的选项
+                collapsable: false, // 不折叠
+                children: [
+                    {title: "vuepress Github Actions", path: "/note/blog/blog_0001"}
+                ],
+            }
+            ,
+            {
+                title: "电子书导航",
+                path: '/note/book/book_0001', // 默认激活的选项
+                collapsable: false, // 不折叠
+                children: [
+                    {title: "《JAVA核心面试知识整理》", path: "/note/book/book_0001"}
+                ],
+            }
+        ]
+    }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,5 @@
-# 采用vuePress+GitHub Actions实现自动化测试的md文档阅读器
+---
+title: 采用vuePress+GitHub Actions实现自动化测试的md文档阅读器
+author: xds
+date: '2023-02-21'
+---
